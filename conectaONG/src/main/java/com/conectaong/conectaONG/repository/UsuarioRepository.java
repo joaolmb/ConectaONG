bbcb0278da6a2f78fcfa7794567db9ae5f3849cb
joaolmb <joaolmb2@gmail.com>
2022-06-15 15:32:45 -0300

    testes

--- conectaONG/src/main/java/com/conectaong/conectaONG/repository/UsuarioRepository.java
+++ conectaONG/src/main/java/com/conectaong/conectaONG/repository/UsuarioRepository.java
@@ -1,5 +1,6 @@
 package com.conectaong.conectaONG.repository;
 
+import java.util.List;
 import java.util.Optional;
 
 import org.springframework.data.jpa.repository.JpaRepository;
@@ -8,4 +9,6 @@ import com.conectaong.conectaONG.model.Usuario;
 
 public interface UsuarioRepository extends JpaRepository<Usuario, Long>{
 	public Optional<Usuario>findByUsuario(String usuario);
+	
+	public List<Usuario> findAllByNomeContainingIgnoreCase(String Usuario);
 }
---
+++ conectaONG/src/test/java/com/conectaong/conectaONG/repository/UsuarioRepositoryTest.java
@@ -0,0 +1,60 @@
+package com.conectaong.conectaONG.repository;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestInstance;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import com.conectaong.conectaONG.model.Usuario;
+
+@SpringBootTest
+@TestInstance(TestInstance.Lifecycle.PER_CLASS)
+public class UsuarioRepositoryTest {
+	
+	@Autowired
+	private UsuarioRepository usuarioRepository;
+	
+	@BeforeAll
+	void start() {
+		usuarioRepository.deleteAll();
+		
+		usuarioRepository.save(new Usuario(0L, "João Lucas", "joao@lucas", "fotoAqui", "123456789"));
+		usuarioRepository.save(new Usuario(0L, "João Vitor", "joao@vitor", "fotoAqui", "123456789"));
+		usuarioRepository.save(new Usuario(0L, "João Gabriel", "joao@gabriel", "fotoAqui", "123456789"));
+		usuarioRepository.save(new Usuario(0L, "David Lucas", "david@lucas", "fotoAqui", "123456789"));
+	}
+	
+	@Test
+	@DisplayName("Retorna um usuário")
+	public void deveRetornarUmUsuario() {
+		Optional<Usuario> usuario = usuarioRepository.findByUsuario("joao@lucas");
+		assertTrue(usuario.get().getUsuario().equals("joao@lucas"));
+	}
+	
+	@Test
+	@DisplayName("Retorna 3 usuários")
+	public void deveretornarTresUsuarios() {
+		List<Usuario> listaDeUsuarios = usuarioRepository.findAllByNomeContainingIgnoreCase("joao");
+		
+		assertEquals(3, listaDeUsuarios.size());
+		
+		assertTrue(listaDeUsuarios.get(0).getNome().equals("João Lucas"));
+		assertTrue(listaDeUsuarios.get(1).getNome().equals("João Vitor"));
+		assertTrue(listaDeUsuarios.get(2).getNome().equals("Joao Gabriel"));
+	}
+	
+	@AfterAll
+	public void end() {
+		usuarioRepository.deleteAll();
+	}
+
+}
